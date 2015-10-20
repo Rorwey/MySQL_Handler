3d2f029532ca4f3bfab3c53c1d3d89379100b863
zokjebi <myimgman@gmail.com>
2015-10-20 18:31:53 +0900

    2015-10-20-01 최초 업로드
    WPF(C#), MySql 연동 최초 작성

---
+++ MysqlSample/MysqlSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MysqlSample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MySqlConn db = new MySqlConn();
+            db.Connect();
+            db.Execute("SELECT * FROM fix_register");
+            
+
+            db.DisConnect();
+
+        }
+    }
+}
---
+++ MysqlSample/MysqlSample/MySqlConn.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace MysqlSample
+{
+    class MySqlConn
+    {
+        // MySql DB 설정 및 세팅
+        string strConn = "SERVER=localhost; DATABASE=test; UID=root; PWD=admin; CHARSET=utf8;";
+        MySqlConnection con = new MySqlConnection();
+
+        public int Connect()
+        {
+            try
+            {
+                con.ConnectionString = strConn;
+                con.Open();
+                return 0;
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+            
+        }
+
+        public void DisConnect()
+        {
+            try
+            {
+                if (con != null)
+                {
+                    con.Close();
+                    con = null;
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        public void Insert(string query)
+        {
+            string _query;
+            // 샘플 쿼리
+            //_query = "INSERT ...";
+            _query = query;
+
+            MySqlCommand cmd = new MySqlCommand(_query, con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+
+        public void Execute(string query)
+        {
+            string _query;
+            // 샘플 쿼리
+            //query = "SELECT * FROM fix_register";
+            _query = query;
+
+            MySqlCommand cmd = new MySqlCommand(_query, con);
+            try
+            {
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    //Console.WriteLine(reader.GetString(0));
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+   
+    }
+
+   
+}
